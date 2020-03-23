e3c875c05726c856fad528a205733792a1367244
ioanapravai <ioana.pravai99@gmail.com>
2020-03-23 14:30:59 +0200

    linear and ridge regression in lab6

--- laborator6/Car.py
+++ laborator6/Car.py
@@ -30,7 +30,30 @@ train1, test1 = normalize(training_data_1, training_data_2)
 train2, test2 = normalize(training_data_2, training_data_3)
 train3, test3 = normalize(training_data_1, training_data_3)
 
+linear_regression_model = LinearRegression()
 ridge_regression_model = Ridge()
+
+linear_regression_model.fit(train1, prices_1)
+pred1_linear = linear_regression_model.predict(test1)
+mse_value1_linear = mean_squared_error(prices_1, pred1_linear)
+mae_value1_linear = mean_absolute_error(prices_1, pred1_linear)
+
+linear_regression_model.fit(train2, prices_2)
+pred2_linear = linear_regression_model.predict(test2)
+mse_value2_linear = mean_squared_error(prices_2, pred2_linear)
+mae_value2_linear = mean_absolute_error(prices_2, pred2_linear)
+
+linear_regression_model.fit(train3, prices_3)
+pred3_linear = linear_regression_model.predict(test3)
+mse_value3_linear = mean_squared_error(prices_3, pred3_linear)
+mae_value3_linear = mean_absolute_error(prices_3, pred3_linear)
+
+mean_mse_value_linear = (mse_value1_linear + mse_value2_linear + mse_value3_linear)/3
+mean_mae_value_linear = (mae_value1_linear + mae_value2_linear + mae_value3_linear)/3
+
+print("2. MAE & MSE values: ")
+print(mean_mae_value_linear, mean_mse_value_linear)
+
 ridge_regression_model.fit(train1, prices_1)
 pred1 = ridge_regression_model.predict(test1)
 mse_value1 = mean_squared_error(prices_1, pred1)
@@ -47,7 +70,8 @@ pred3 = ridge_regression_model.predict(test3)
 mse_value3 = mean_squared_error(prices_3, pred3)
 mae_value3 = mean_absolute_error(prices_3, pred3)
 
-mean_mse_value = (mse_value1 + mse_value2 + mse_value3)/3
-mean_mae_value = (mae_value1 + mae_value2 + mae_value3)/3
+mean_mse_value_ridge = (mse_value1 + mse_value2 + mse_value3)/3
+mean_mae_value_ridge = (mae_value1 + mae_value2 + mae_value3)/3
 
-print(mean_mae_value, mean_mse_value)
+print("3. MAE & MSE values: ")
+print(mean_mae_value_ridge, mean_mse_value_ridge)
